03b113184d61dc9d1fdb9f98743f142e2dfd70f7
李永磊 <liylo@chanjet.com>
2018-05-17 16:52:08 +0800

    解决ie浏览器兼容性问题

---
+++ website/dist/0.9bd8.bundle.js
@@ -0,0 +1,174 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/home/Home.jsx":
+/*!***************************!*\
+  !*** ./src/home/Home.jsx ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = undefined;
+
+var _getPrototypeOf = __webpack_require__(/*! babel-runtime/core-js/object/get-prototype-of */ "./node_modules/babel-runtime/core-js/object/get-prototype-of.js");
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _classCallCheck2 = __webpack_require__(/*! babel-runtime/helpers/classCallCheck */ "./node_modules/babel-runtime/helpers/classCallCheck.js");
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = __webpack_require__(/*! babel-runtime/helpers/createClass */ "./node_modules/babel-runtime/helpers/createClass.js");
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _possibleConstructorReturn2 = __webpack_require__(/*! babel-runtime/helpers/possibleConstructorReturn */ "./node_modules/babel-runtime/helpers/possibleConstructorReturn.js");
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = __webpack_require__(/*! babel-runtime/helpers/inherits */ "./node_modules/babel-runtime/helpers/inherits.js");
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactSidenav = __webpack_require__(/*! react-sidenav2 */ "./node_modules/react-sidenav2/dist/index.js");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Home = function (_React$Component) {
+    (0, _inherits3.default)(Home, _React$Component);
+
+    function Home() {
+        (0, _classCallCheck3.default)(this, Home);
+        return (0, _possibleConstructorReturn3.default)(this, (Home.__proto__ || (0, _getPrototypeOf2.default)(Home)).apply(this, arguments));
+    }
+
+    (0, _createClass3.default)(Home, [{
+        key: "render",
+        value: function render() {
+            return _react2.default.createElement(
+                "div",
+                null,
+                _react2.default.createElement(
+                    "strong",
+                    null,
+                    "react \u5BFC\u822A\u7EC4\u4EF6"
+                ),
+                _react2.default.createElement(
+                    "ul",
+                    null,
+                    _react2.default.createElement(
+                        "li",
+                        null,
+                        _react2.default.createElement(
+                            "a",
+                            { href: "javascript:;", onClick: function onClick() {
+                                    (0, _reactSidenav.jumpToById)({ "routeId": "0" });
+                                } },
+                            "\u5F00\u59CB\u4F7F\u7528"
+                        )
+                    ),
+                    _react2.default.createElement(
+                        "li",
+                        null,
+                        _react2.default.createElement(
+                            "a",
+                            { href: "javascript:;", onClick: function onClick() {
+                                    (0, _reactSidenav.jumpToById)({ "routeId": "1" });
+                                } },
+                            "\u914D\u7F6E"
+                        )
+                    ),
+                    _react2.default.createElement(
+                        "li",
+                        null,
+                        _react2.default.createElement(
+                            "a",
+                            { href: "javascript:;", onClick: function onClick() {
+                                    (0, _reactSidenav.jumpToById)({ "routeId": "2" });
+                                } },
+                            "Sidenav"
+                        ),
+                        "\u4FA7\u680F\u5BFC\u822A\u7EC4\u4EF6"
+                    ),
+                    _react2.default.createElement(
+                        "li",
+                        null,
+                        _react2.default.createElement(
+                            "a",
+                            { href: "javascript:;", onClick: function onClick() {
+                                    (0, _reactSidenav.jumpToById)({ "routeId": "3" });
+                                } },
+                            "SidenavPanel"
+                        ),
+                        "\u5185\u5BB9\u9762\u677F\u7EC4\u4EF6"
+                    ),
+                    _react2.default.createElement(
+                        "li",
+                        null,
+                        _react2.default.createElement(
+                            "a",
+                            { href: "javascript:;", onClick: function onClick() {
+                                    (0, _reactSidenav.jumpToById)({ "routeId": "4" });
+                                } },
+                            "Topnav"
+                        ),
+                        "\u9876\u90E8\u5BFC\u822A\u7EC4\u4EF6"
+                    ),
+                    _react2.default.createElement(
+                        "li",
+                        null,
+                        _react2.default.createElement(
+                            "a",
+                            { href: "javascript:;", onClick: function onClick() {
+                                    (0, _reactSidenav.jumpToById)({ "routeId": "5" });
+                                } },
+                            "Crumbnav"
+                        ),
+                        "\u9762\u5305\u5C51\u5BFC\u822A\u7EC4\u4EF6"
+                    ),
+                    _react2.default.createElement(
+                        "li",
+                        null,
+                        _react2.default.createElement(
+                            "a",
+                            { href: "javascript:;", onClick: function onClick() {
+                                    (0, _reactSidenav.jumpToById)({ "routeId": "6" });
+                                } },
+                            "jumpTo"
+                        ),
+                        "\u9875\u9762\u8DF3\u8F6C\u65B9\u6CD5"
+                    ),
+                    _react2.default.createElement(
+                        "li",
+                        null,
+                        _react2.default.createElement(
+                            "a",
+                            { href: "javascript:;", onClick: function onClick() {
+                                    (0, _reactSidenav.jumpToById)({ "routeId": "7" });
+                                } },
+                            "jumpToById"
+                        ),
+                        "\u6839\u636Eid\u8DF3\u8F6C\u65B9\u6CD5"
+                    )
+                )
+            );
+        }
+    }]);
+    return Home;
+}(_react2.default.Component);
+
+exports.default = Home;
+
+/***/ })
+
+}]);
+//# sourceMappingURL=0.9bd8.bundle.js.map
---
+++ website/dist/7.0609.bundle.js
@@ -0,0 +1,138 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[7],{
+
+/***/ "./src/sidenav-option/modules/Loading/Loading.jsx":
+/*!********************************************************!*\
+  !*** ./src/sidenav-option/modules/Loading/Loading.jsx ***!
+  \********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = undefined;
+
+var _getPrototypeOf = __webpack_require__(/*! babel-runtime/core-js/object/get-prototype-of */ "./node_modules/babel-runtime/core-js/object/get-prototype-of.js");
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _classCallCheck2 = __webpack_require__(/*! babel-runtime/helpers/classCallCheck */ "./node_modules/babel-runtime/helpers/classCallCheck.js");
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = __webpack_require__(/*! babel-runtime/helpers/createClass */ "./node_modules/babel-runtime/helpers/createClass.js");
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _possibleConstructorReturn2 = __webpack_require__(/*! babel-runtime/helpers/possibleConstructorReturn */ "./node_modules/babel-runtime/helpers/possibleConstructorReturn.js");
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = __webpack_require__(/*! babel-runtime/helpers/inherits */ "./node_modules/babel-runtime/helpers/inherits.js");
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactSidenav = __webpack_require__(/*! react-sidenav2 */ "./node_modules/react-sidenav2/dist/index.js");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Loading = function (_React$Component) {
+    (0, _inherits3.default)(Loading, _React$Component);
+
+    function Loading() {
+        (0, _classCallCheck3.default)(this, Loading);
+        return (0, _possibleConstructorReturn3.default)(this, (Loading.__proto__ || (0, _getPrototypeOf2.default)(Loading)).apply(this, arguments));
+    }
+
+    (0, _createClass3.default)(Loading, [{
+        key: "render",
+        value: function render() {
+            return _react2.default.createElement(
+                "div",
+                null,
+                _react2.default.createElement(
+                    "strong",
+                    null,
+                    "\u63CF\u8FF0"
+                ),
+                _react2.default.createElement(
+                    "pre",
+                    null,
+                    _react2.default.createElement(
+                        "code",
+                        null,
+                        "tab\u9875\u52A0\u8F7D\u65F6\u7684\u8FC7\u6E21loading\u9762\u677F"
+                    )
+                ),
+                _react2.default.createElement(
+                    "strong",
+                    null,
+                    "\u751F\u6210Loading"
+                ),
+                _react2.default.createElement(
+                    "pre",
+                    null,
+                    _react2.default.createElement(
+                        "code",
+                        null,
+                        "\nconst Loading=({navData})=>{\n    return (\n        <div>\n            {navData.name}\u9875\u52A0\u8F7D\u4E2D...\n        </div>\n    )\n};\n"
+                    )
+                ),
+                _react2.default.createElement(
+                    "strong",
+                    null,
+                    "\u5C06Loading\u4F20\u5165\u914D\u7F6E\u4E2D"
+                ),
+                _react2.default.createElement(
+                    "pre",
+                    null,
+                    _react2.default.createElement(
+                        "code",
+                        null,
+                        "\nsetup({\n    ...\n    Loading: Loading,\n    ...\n});\n"
+                    )
+                ),
+                _react2.default.createElement(
+                    "strong",
+                    null,
+                    "\u53C2\u6570"
+                ),
+                _react2.default.createElement(
+                    "pre",
+                    null,
+                    _react2.default.createElement(
+                        "code",
+                        null,
+                        "navData:any \u5728",
+                        _react2.default.createElement(
+                            "a",
+                            {
+                                href: "javascript:;",
+                                onClick: function onClick() {
+                                    return (0, _reactSidenav.jumpToById)({ "routeId": "10" });
+                                }
+                            },
+                            "Data"
+                        ),
+                        "\u5B9A\u4E49\u7684\u5BF9\u5E94\u7684\u5BFC\u822A\u6570\u636E"
+                    )
+                )
+            );
+        }
+    }]);
+    return Loading;
+}(_react2.default.Component);
+
+exports.default = Loading;
+
+/***/ })
+
+}]);
+//# sourceMappingURL=7.0609.bundle.js.map
